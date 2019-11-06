221e8ad02443694d22dec72af1978f7362d3a67b
Zakeria Ali <zak.fined@gmail.com>
2019-11-06 22:23:13 +0000

    Log the image url for the coin

--- src/Components/CoinGrid/CoinImage.js
+++ src/Components/CoinGrid/CoinImage.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 const CoinImage = ({image, symbol, customStyling}) => {
+	console.log(image)
 	return (
 		<img src={`http://cryptocompare.com/${image}`} alt={symbol} style={customStyling || {height: '50px'}} />
 	)
