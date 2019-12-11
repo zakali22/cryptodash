edfc0e236b5351f660a3681c5bbfd44de04c15b9
Zak Ali <zak.ali@thetin.net>
2019-12-11 14:59:20 +0000

    Updates

---
+++ precache-manifest.05c3e0353acca6e174d34fad4bb2fc60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d13cad636ef089c2abd4662340366d9",
+    "url": "/cryptodash/index.html"
+  },
+  {
+    "revision": "ccb19f1aadca0b174185",
+    "url": "/cryptodash/static/css/main.bcbbbb28.chunk.css"
+  },
+  {
+    "revision": "c3e1f95872c28df70099",
+    "url": "/cryptodash/static/js/2.cc57d1c4.chunk.js"
+  },
+  {
+    "revision": "ccb19f1aadca0b174185",
+    "url": "/cryptodash/static/js/main.92a25c02.chunk.js"
+  },
+  {
+    "revision": "79f2220369cb1304cd8a",
+    "url": "/cryptodash/static/js/runtime-main.605224ff.js"
+  }
+]);
